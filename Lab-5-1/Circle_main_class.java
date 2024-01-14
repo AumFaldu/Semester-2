91c82afbfa20e3b63aaf60625a09328e17c5d4e2
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-01-14 13:41:31 +0530

    Update Circle_main_class.java

--- Lab-5-1/Circle_main_class.java
+++ Lab-5-1/Circle_main_class.java
@@ -4,7 +4,7 @@ public class Circle_main_class
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		Circle ar = new Circle();
+		Circle_class ar = new Circle_class();
 		System.out.println("Enter radius of circle : ");
 		int r = sc.nextInt();
 		double res = ar.area(r);
