0226e2c2435079f29230a028daeb9c67061672e7
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-01-16 10:27:22 +0530

    Update Lab_4_6.java

--- Lab_4_6.java
+++ Lab_4_6.java
@@ -1,13 +1,20 @@
 public class Lab_4_6
 {
-	public static void main(String[] args) {
-        String str = args[0];
-        int s = str.charAt(0); 
-        if(s>=65 && s<=91){
-            System.out.println(str);
-        }
-        else{
-            System.out.println("Error Occured");
+	public static void main(String[] args) 
+    {
+        for(int i=0;i<args.length;i++) 
+        {
+
+            if(args[i].charAt(0)>=65 &&  args[i].charAt(0)<=91)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println("Error occured");
+                break;
+            }
+        
         }
     }
 }
