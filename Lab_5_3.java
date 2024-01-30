b3b9c1695828ea26a6af6e86ee61993c121ea3f4
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-01-30 17:01:22 +0530

    Update Lab_5_3.java

--- Lab_5_3.java
+++ Lab_5_3.java
@@ -3,55 +3,65 @@ public class Lab_5_3
 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-
-		Count_vowel count = new Count_vowel(s);
-
+		Sentence sentence = new Sentence();
+		do
+		{
+			System.out.println("Enter a sentence(type quit to exit)");
+			String s = sc.nextLine();
+			if(s.equals("quit"))
+			{
+				break;
+			}
+			sentence.processSentence(s);
+		}while(true);
+		System.out.println("a = "+sentence.count_a+"\ne = "+sentence.count_e+"\ni = "+sentence.count_i+"\no = "+sentence.count_o+"\nu = "+sentence.count_u);
 	}
 }
-class Count_vowel{
+class Sentence{
 	String s;
 	int count_a=0;
 	int count_e=0;
 	int count_i=0;
 	int count_o=0;
 	int count_u=0;
-	Count_vowel(String s)
+	Sentence()
 	{
+
 		this.s = s;
-		this.count_a;
-		this.count_e;
-		this.count_i;
-		this.count_o;
-		this.count_u;
-		for(int count=0;count<s.length();count++)
+		this.count_a=count_a;
+		this.count_e=count_e;
+		this.count_i=count_i;
+		this.count_o=count_o;
+		this.count_u=count_u;
+	}
+		public void processSentence(String s)
 		{
-			if(s.charAt(i)=="a"||s.charAt(i)=="A")
+
+		for(int i=0;i<s.length();i++)
+		{
+
+			if(s.charAt(i)=='a'||s.charAt(i)=='A')
 			{
 				count_a++;
 			}
-			if(s.charAt(i)=="e"||s.charAt(i)=="E")
+			if(s.charAt(i)=='e'||s.charAt(i)=='E')
 			{
 				count_e++;
 			}
-			if(s.charAt(i)=="i"||s.charAt(i)=="I")
+			if(s.charAt(i)=='i'||s.charAt(i)=='I')
 			{
 				count_i++;
 			}
-			if(s.charAt(i)=="o"||s.charAt(i)=="O")
+			if(s.charAt(i)=='o'||s.charAt(i)=='O')
 			{
 				count_o++;
 			}
-			if(s.charAt(i)=="u"||s.charAt(i)=="U")
+			if(s.charAt(i)=='u'||s.charAt(i)=='U')
 			{
 				count_u++;
 			}
 		}
-		return count;
-		return count_a;
-		return count_e;
-        return count_i;
-		return count_o;
-		return count_u;
+		}
+
 	}
-}	
+
