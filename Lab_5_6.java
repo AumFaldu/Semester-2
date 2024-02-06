f1f9aa90de7658f6a24cfbca7187fa7228932b10
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-02-06 10:23:23 +0530

    Add files via upload

---
+++ Lab_5_6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Lab_5_6
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter how many objects to be created : ");
+		int n = sc.nextInt();
+		Counter c[] = new Counter[n];
+		for(int i=0;i<n;i++)
+		{
+			 c[i] = new Counter();
+		}
+
+		System.out.println("Total objects created = "+c[0].count);
+	}
+}
+class Counter{
+	static int count;
+	Counter()
+	{
+		count++;
+	}
+}
