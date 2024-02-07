655fe653d01fb3a7994f84b6e3c3a37abfaf01ec
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-02-07 15:53:35 +0530

    Add files via upload

---
+++ Extra_Distance.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Extra_Distance{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter coordinates of d1 : ");
+		double x1 = sc.nextDouble();
+		double y1 = sc.nextDouble();
+		System.out.println("Enter coordinates of d2 : ");
+		double x2 = sc.nextDouble();
+		double y2 = sc.nextDouble();
+		Distance d1 = new Distance(x1,y1);
+		Distance d2 = new Distance(x2,y2);
+		System.out.println("Distance = "+Distance.formula(x2-x1,y2-y1));
+	}
+}
+class Distance{
+	double x;
+	double y;
+	static double dist;
+	Distance(double x,double y)
+	{
+		this.x = x;
+		this.y = y;
+		this.dist = dist;
+	}
+	public static double formula(double x,double y)
+	{
+		double dist = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+		return dist;
+	}
+}
