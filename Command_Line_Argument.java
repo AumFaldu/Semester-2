2723e5051b867b36249f8195dbb5dedec444e739
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-02-11 11:54:24 +0530

    Update Command_Line_Argument.java

--- Command_Line_Argument.java
+++ Command_Line_Argument.java
@@ -2,7 +2,11 @@ public class Command_Line_Argument
 {
 	public static void main(String[] args)
 	{
-		String s = args[0];
+		String s = new String();
+		for(int i=0;i<args.length;i++)
+		{
+			s += args[i];
+		}
 		System.out.println(s);
 	}
 }
