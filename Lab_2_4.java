6cbd186011c1435f08a7e2a7cfacde8f2a85753c
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-02-11 11:59:32 +0530

    Update Lab_2_4.java

--- Lab_2_4.java
+++ Lab_2_4.java
@@ -4,10 +4,9 @@ public class Lab_2_4
 	public  static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		double pi = 3.14;
 		System.out.print("Enter radius of circle : ");
 		double r = sc.nextDouble();
-		System.out.println("Area of Circle = "+(pi*r*r));
+		System.out.println("Area of Circle = "+(Math.PI*Math.pow(r,2)));
 		sc.close();
 	}
 }
