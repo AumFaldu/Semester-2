efce36744d74b0b6505d3d7c4df0660afae9da2f
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2024-03-12 16:26:14 +0530

    Update Lab_7_2.java

--- Lab_7_2.java
+++ Lab_7_2.java
@@ -1,25 +1,37 @@
-public class Lab_7_2{
-	public static void main(String[] args) {
-		B display = new B();
-		display.display_P1();
-		display.display_P2();
-	}
+interface A {
+    int CONSTANT_A = 10;
+    void methodA();
 }
-interface P1{
-	int a=1;
-	public void display_P1();
+interface A1 extends A {
+    int CONSTANT_A1 = 20;
+    void methodA1();
 }
-interface P2{
-	int b=2;
-	public void display_P2();
+interface A2 extends A {
+    int CONSTANT_A2 = 30;
+    void methodA2();
 }
-class B implements P1,P2{
-	public void display_P1()
-	{
-		System.out.println("Value of a = "+P1.a);
-	}
-	public void display_P2()
-	{
-		System.out.println("Value of b = "+P2.b);
-	}
+interface A12 extends A1, A2 {
+    int CONSTANT_A12 = 40;
+}
+class B implements A12 {
+    public void methodA() {
+        System.out.println("Constant A: " + CONSTANT_A);
+    }
+
+    public void methodA1() {
+        System.out.println("Constant A1: " + CONSTANT_A1);
+    }
+
+    public void methodA2() {
+        System.out.println("Constant A2: " + CONSTANT_A2);
+    }
+}
+
+public class Lab_7_2 {
+    public static void main(String[] args) {
+        B b = new B();
+        b.methodA(); 
+        b.methodA1();
+        b.methodA2();
+    }
 }
